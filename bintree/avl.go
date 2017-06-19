3957344b97caf9d6050fddadeb7a92ca9f76edcc
emersion <contact@emersion.fr>
2017-06-19 09:14:46 +0200

    bintree: implement self-balancing AVL

--- bintree/avl.go
+++ bintree/avl.go
@@ -2,14 +2,75 @@ package bintree
 
 type LessFunc func(a, b interface{}) bool
 
+type node struct {
+	value interface{}
+	h int
+	left, right *node
+}
+
+func (n *node) height() int {
+	if n == nil {
+		return 0
+	}
+	return n.h
+}
+
+func (n *node) computeHeight() {
+	hl, hr := n.left.height(), n.right.height()
+
+	h := hl
+	if hr > hl {
+		h = hr
+	}
+	n.h = 1 + h
+}
+
+// Assumes n != nil && n.left != nil
+func (n *node) rotateRight() *node {
+	l := n.left
+	l.right, n.left = n, l.right
+	n.computeHeight()
+	l.computeHeight()
+	return l
+}
+
+// Assumes n != nil && n.right != nil
+func (n *node) rotateLeft() *node {
+	r := n.right
+	n.right, r.left = r.left, n
+	n.computeHeight()
+	r.computeHeight()
+	return r
+}
+
+func (n *node) balance() *node {
+	if n == nil {
+		return nil
+	}
+
+	l, r := n.left, n.right
+	hl, hr := l.height(), r.height()
+	if hl > hr + 1 {
+		ll, lr := l.left, l.right
+		if ll.height() < lr.height() {
+			n.left = l.rotateLeft()
+		}
+		return n.rotateRight()
+	} else if hr > hl + 1 {
+		rl, rr := r.left, r.right
+		if rr.height() < rl.height() {
+			n.right = r.rotateRight()
+		}
+		return n.rotateLeft()
+	} else {
+		n.computeHeight()
+		return n
+	}
+}
+
 type AVL struct {
 	less LessFunc
 	root *node
-}
-
-type node struct {
-	value interface{}
-	right, left *node
 }
 
 func NewAVL(less LessFunc) *AVL {
@@ -60,9 +121,5 @@ func (avl *AVL) Append(v interface{}) {
 		}
 	}
 
-	avl.balance()
-}
-
-func (avl *AVL) balance() {
-	// TODO
+	avl.root = avl.root.balance()
 }
